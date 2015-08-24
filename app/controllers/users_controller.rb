dca858426bb906658e7ab733b9b11a6d1d596bea
Cameron Aziz <cameronaziz@Camerons-MacBook-Pro.local>
2015-08-23 23:38:19 -0700

    links to backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
   before_action only: [:index, :new, :show, :create, :edit, :update, :destroy] do
     authenticate_user_and_group(['Administrators', 'Managers'], false)
   end
+  
+  skip_before_action :authenticate_user, only: :register
+
 
   def index
     @users = User.all
